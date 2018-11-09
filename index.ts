4a1a91ddc438494f7047903adfcc3231c18f68f1
Rob Bateman <rob@infiniteturtles.co.uk>
2018-11-09 14:17:15 +0000

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-console.log("AwayJS - Stage - 0.9.13");
+console.log("AwayJS - Stage - 0.9.14");
 export {AGALMiniAssembler} from "./lib/aglsl/assembler/AGALMiniAssembler";
 export {Flags} from "./lib/aglsl/assembler/Flags";
 export {FS} from "./lib/aglsl/assembler/FS";
