0e6a094b04ad0c0da08954c1c21492e4d8d4aa1f
80prozent <80prozent@differentdesign.de>
2019-04-24 14:48:07 +0200

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-console.log("AwayJS - Stage - 0.10.2");
+console.log("AwayJS - Stage - 0.10.3");
 export {AGALMiniAssembler} from "./lib/aglsl/assembler/AGALMiniAssembler";
 export {Flags} from "./lib/aglsl/assembler/Flags";
 export {FS} from "./lib/aglsl/assembler/FS";
