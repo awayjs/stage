6ec5163b708df3cfb9c32451da42ebed8fed67e1
80prozent <80prozent@differentdesign.de>
2019-06-27 13:40:41 +0200

    switch version log to console.debug

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-console.log("AwayJS - Stage - 0.10.5");
+console.debug("AwayJS - Stage - 0.10.5");
 export {AGALMiniAssembler} from "./lib/aglsl/assembler/AGALMiniAssembler";
 export {Flags} from "./lib/aglsl/assembler/Flags";
 export {FS} from "./lib/aglsl/assembler/FS";
