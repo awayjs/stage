0aed70ee091b86af90694fea984c3b9bf693067e
Rob Bateman <rob.bateman@gmail.com>
2020-03-20 00:18:40 +0000

    add threshold filter

---
+++ lib/filters/ThresholdFilter3D.ts
@@ -0,0 +1,105 @@
+import {Filter3DThresholdTask} from "./tasks/Filter3DThresholdTask";
+
+import {Filter3DBase} from "./Filter3DBase";
+import { Image2D } from '../image/Image2D';
+import { Rectangle, Point } from '@awayjs/core';
+
+export class ThresholdFilter3D extends Filter3DBase
+{
+	private _thresholdTask:Filter3DThresholdTask;
+	
+	/**
+	 * Creates a new ThresholdFilter3D object
+	 * @param blurX The amount of horizontal blur to apply
+	 * @param blurY The amount of vertical blur to apply
+	 * @param stepSize The distance between samples. Set to -1 to autodetect with acceptable quality.
+	 */
+	constructor()
+	{
+		super();
+
+		this._thresholdTask = new Filter3DThresholdTask();
+
+		this.addTask(this._thresholdTask);
+	}
+	
+	public get rect():Rectangle
+	{
+		return this._thresholdTask.rect;
+	}
+	
+	public set rect(value:Rectangle)
+	{
+		this._thresholdTask.rect = value;
+	}
+	
+	public get destPoint():Point
+	{
+		return this._thresholdTask.destPoint;
+	}
+	
+	public set destPoint(value:Point)
+	{
+		this._thresholdTask.destPoint = value;
+	}
+
+	public get operation():string
+	{
+		return this._thresholdTask.operation;
+	}
+	
+	public set operation(value:string)
+	{
+		this._thresholdTask.operation = value;
+	}
+
+	public get threshold():number
+	{
+		return this._thresholdTask.threshold;
+	}
+	
+	public set threshold(value:number)
+	{
+		this._thresholdTask.threshold = value;
+	}
+	
+	public get color():number
+	{
+		return this._thresholdTask.color;
+	}
+	
+	public set color(value:number)
+	{
+		this._thresholdTask.color = value;
+	}
+	
+	public get mask():number
+	{
+		return this._thresholdTask.mask;
+	}
+	
+	public set mask(value:number)
+	{
+		this._thresholdTask.mask = value;
+	}
+		
+	public get copySource():boolean
+	{
+		return this._thresholdTask.copySource;
+	}
+	
+	public set copySource(value:boolean)
+	{
+		this._thresholdTask.copySource = value;
+	}
+
+	public get sourceTexture():Image2D
+	{
+		return this._thresholdTask.sourceTexture;
+	}
+
+	public set sourceTexture(value:Image2D)
+	{
+		this._thresholdTask.sourceTexture = value;
+	}
+}
