0e5012727fc12ab2299de246c9e2e5f930d48a78
Exponenta <rondo.devil@gmail.com>
2020-06-19 10:05:26 +0300

    Comments

--- lib/webgl/ContextWebGLFlags.ts
+++ lib/webgl/ContextWebGLFlags.ts
@@ -5,22 +5,40 @@ export const enum ContextWebGLVersion {
 }
 
 export const enum ContextWebGLPreference {
-    // not allowed
+    /**
+     * Not allowed
+     */
     NONE = 0,
-    // for POT and nPOT textures or on WebGL2
-    ALL = 1,
-    // only for POT
-    POT = 2,
+    /**
+     * For POT and nPOT textures
+     */
+    ALL_TEXTURES = 1,
+    /**
+     * Only for POT textures or WebGl2
+    */
+    POT_TEXTURES = 2,
 }
 
 export const ContextWebGLFlags = {
-    // preferred  WebGL version for rendering
+    /**
+     * Preferred  WebGL version for rendering
+     * @default WEBGL2
+     */
     PREF_VERSION: ContextWebGLVersion.WEBGL2,
-    // preferred  multisample for webgl2 on rendertargets. 
+    /**
+     * Preferred  multisample for webgl2 on rendertargets.
+     * @default true
+     */
     PREF_MULTISAMPLE: true,
-    // allow mipmaps for all textures
-    PREF_MIPMAP: ContextWebGLPreference.ALL,
-    // preferred repeate wrapping for all type of textures, on webg1 will throw a warn for nPOT textures
-    // ContextWebGLPreference.POT will allow only for POT textures on WebGL1
-    PREF_REPEAT_WRAP: ContextWebGLPreference.ALL
+    /**
+     * Preferred mipmaps for textures
+     * @default ALL
+     */
+    PREF_MIPMAP: ContextWebGLPreference.ALL_TEXTURES,
+    /**
+     * Preferred repeate wrapping for all type of textures, on webg1 will throw a warn for nPOT textures
+     * ContextWebGLPreference.POT_TEXTURES will allow only for POT textures on WebGL1
+     * @default ALL_TEXTURES
+     */
+    PREF_REPEAT_WRAP: ContextWebGLPreference.ALL_TEXTURES
 }
