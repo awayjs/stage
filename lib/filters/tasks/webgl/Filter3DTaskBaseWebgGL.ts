ebacf5f8f05b54d267ae00bd3db180af58d57fce
Exponenta <rondo.devil@gmail.com>
2020-08-03 14:45:31 +0300

    WIP. Native FilterBase

--- lib/filters/tasks/webgl/Filter3DTaskBaseWebgGL.ts
+++ lib/filters/tasks/webgl/Filter3DTaskBaseWebgGL.ts
@@ -2,6 +2,35 @@ import { Filter3DTaskBase } from '../Filter3DTaskBase';
 import { Stage } from '../../../Stage';
 import { ProgramWebGL } from '../../../webgl/ProgramWebGL';
 
+const VERTEX_DEF = `
+precision highp float;
+uniform float yflip;
+attribute vec4 aPosition;
+attribute vec4 aUv;
+
+varying vec4 vUv;
+
+void main() {
+	vUv = aUv;
+	vec4 pos = aPosition;
+	pos.z = pos.z * 2.0 - pos.w;
+	gl_Position = pos;
+}
+`;
+
+const FRAG_DEF = `
+precision highp float;
+uniform vec4 fc[2];
+uniform sampler2D uSampler0;
+varying vec4 vUv;
+
+void main() {
+    vec4 color = texture2D(uSampler0, vUv.xy);
+    gl_FragColor = color;
+}
+
+`;
+
 export class Filter3DTaskBaseWebGL extends Filter3DTaskBase
 {
 	public updateProgram(stage:Stage):void
@@ -17,5 +46,13 @@ export class Filter3DTaskBaseWebGL extends Filter3DTaskBase
 			this.getVertexCode(), 
 			this.getFragmentCode());
 		this._program3DInvalid = false;
+	}
+
+	getVertexCode() {
+		return VERTEX_DEF;
+	}
+	
+	getFragmentCode() {
+		return FRAG_DEF;
 	}
 }
