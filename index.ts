57401bd65eb9d9274c9e5e02a9b39f2f34dcb6d6
Exponenta <rondo.devil@gmail.com>
2020-11-28 13:24:31 +0300

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-console.debug('AwayJS - Stage - 0.11.54');
+console.debug('AwayJS - Stage - 0.11.55');
 
 export { AGALMiniAssembler } from './lib/aglsl/assembler/AGALMiniAssembler';
 export { Flags } from './lib/aglsl/assembler/Flags';
