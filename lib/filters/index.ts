49759d661aa93a51474e07487086eed0b958e0a2
Konstantin Timoshenko <rondo.devil@gmail.com>
2021-03-22 13:22:26 +0300

    WhyNotAssets

--- lib/filters/index.ts
+++ lib/filters/index.ts
@@ -1,3 +1,10 @@
+/**
+ * Why i not transfromed filters to Asset + Abstraction ?
+ * Because this require a big ammout of code. This increase codecount more that at 2 times,
+ * because required listen every props for invalidation, and copy-past data between Asset and abstraction
+ * and then we should again test this, because main problem - invalidate abstaction state.
+ * If you like copy-past - good, you can refact this.
+ */
 export * from './IBitmapFilter';
 export * from './FilterBase';
 export * from './BlurFilter';
