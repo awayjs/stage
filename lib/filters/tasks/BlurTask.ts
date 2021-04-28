73a5135efdb36a250290cf83d6564a269c489034
Konstantin Timoshenko <rondo.devil@gmail.com>
2021-04-28 13:09:11 +0300

    feat: Try implement real gausian blur

--- lib/filters/tasks/BlurTask.ts
+++ lib/filters/tasks/BlurTask.ts
@@ -5,6 +5,17 @@ import { Image2D } from '../../image/Image2D';
 import { TaskBaseWebGL } from './webgl/TaskBaseWebgGL';
 import { ProgramWebGL } from '../../webgl/ProgramWebGL';
 
+const GAUSSIAN_VALUES = {
+	1: [1],
+	2: [0.5, 0.5],
+	3: [0.5, 0.5],
+	5: [0.153388, 0.221461, 0.250301],
+	7: [0.071303, 0.131514, 0.189879, 0.214607],
+	9: [0.028532, 0.067234, 0.124009, 0.179044, 0.20236],
+	11: [0.0093, 0.028002, 0.065984, 0.121703, 0.175713, 0.198596],
+	13: [0.002406, 0.009255, 0.027867, 0.065666, 0.121117, 0.174868, 0.197641],
+	15: [0.000489, 0.002403, 0.009246, 0.02784, 0.065602, 0.120999, 0.174697, 0.197448],
+};
 export class BlurTask extends TaskBaseWebGL /*Filter3DTaskBase*/ {
 	private static MAX_AUTO_SAMPLES: number = 15;
 	public readonly horizontalPass: boolean = true;
@@ -14,6 +25,7 @@ export class BlurTask extends TaskBaseWebGL /*Filter3DTaskBase*/ {
 	private _stepSize: number = 1;
 	private _realStepSize: number;
 	private _blurIndex: number;
+	private _kernel: number = 5;
 
 	public get name() {
 		return `FilterBlurTask:${(this.horizontalPass ? 'hor' : 'vert')}:${this._amount}`;
@@ -28,6 +40,7 @@ export class BlurTask extends TaskBaseWebGL /*Filter3DTaskBase*/ {
 		super();
 
 		this._amount = amount;
+		this._kernel = Math.min(15, amount / 2 | 0 + 1);
 		this._data = new Float32Array([0, 0, 0]);
 		this.stepSize = stepSize;
 		this.horizontalPass = horizonal;
@@ -42,6 +55,7 @@ export class BlurTask extends TaskBaseWebGL /*Filter3DTaskBase*/ {
 			return;
 
 		this._amount = value;
+		this._kernel = Math.min(15, value / 2 | 0 + 1);
 
 		this.invalidateProgram();
 		this.calculateStepSize();
@@ -66,6 +80,7 @@ export class BlurTask extends TaskBaseWebGL /*Filter3DTaskBase*/ {
 
 		let count = 1;
 
+		/*
 		for (let x = this._realStepSize; x <= this._amount; x += this._realStepSize) {
 			body.push(`
 						uv.${hor ? 'x' : 'y'} += uBlurData[1];
@@ -73,6 +88,27 @@ export class BlurTask extends TaskBaseWebGL /*Filter3DTaskBase*/ {
 			`);
 
 			++count;
+		}*/
+
+		const values: number[] = GAUSSIAN_VALUES[this._kernel];
+		const dim = values.length;
+
+		let index = 0;
+		for (let i = 0; i < this._kernel; i++) {
+
+			index = i >= dim ? this._kernel - i - 1 : i;
+
+			let prep = '';
+
+			if (this.horizontalPass) {
+				prep = `${(i - dim + 1).toFixed(1)} * vec2(step, 0.0)`;
+			} else {
+				prep = `${(i - dim + 1).toFixed(1)} * vec2(0.0, step)`;
+			}
+
+			body.push(`
+						color += texture2D(fs0, uv + ${prep}) * ${values[index].toFixed(6)};
+				`);
 		}
 
 		return `
@@ -84,13 +120,12 @@ export class BlurTask extends TaskBaseWebGL /*Filter3DTaskBase*/ {
 			
 			void main() {
 				vec2 uv = vUv.xy;
-				uv.${hor ? 'x' : 'y'} -= uBlurData[0];
-
-				vec4 color = texture2D(fs0, uv);
+				vec4 color = vec4(0.0, 0.0, 0.0, 0.0);
+				float step =  uBlurData[1];
 
 				${body.join('\n')}
 
-				gl_FragColor = color * 1.0 / ${count.toFixed(1)};
+				gl_FragColor = color;
 			}
 		`;
 	}
