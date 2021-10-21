a78e22714b3dbfc41423936aaaba3e1b53b24f00
Konstantin Timoshenko <rondo.devil@gmail.com>
2021-10-21 14:08:15 +0300

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-console.debug('AwayJS - Stage - 0.11.119');
+console.debug('AwayJS - Stage - 0.11.120');
 
 export { AGALMiniAssembler } from './lib/aglsl/assembler/AGALMiniAssembler';
 export { Flags } from './lib/aglsl/assembler/Flags';
